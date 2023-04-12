ab881229437eee0c9bb03c4ee9e68f489add3051
Jalissa Perez <viola@Jalissas-MacBook-Air.local>
2023-04-11 20:06:55 -0400

    Created lists for destinations, restaurants, transportations and entertaiments.

---
+++ main.py
@@ -0,0 +1,7 @@
+destinations=['mexico','disney world','costa rica','europe']
+    
+restaurants=['the south of france', 'pizza shop', 'sushi on me', 'ajo y oregano']
+
+transportations=['airplane', 'jet pack', 'boat','motorbike']
+
+entertainments=['muesuem', 'aquarium','comedy show','broadway show']
