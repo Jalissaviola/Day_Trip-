05d6adb22430b04beeefc89e0ffb6d08b3120646
Jalissa Perez <viola@Jalissas-MacBook-Air.local>
2023-04-11 20:29:59 -0400

    Imported Random function to generate and print a random trip from my lists.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random 
+
 destinations=['mexico','disney world','costa rica','europe']
     
 restaurants=['the south of france', 'pizza shop', 'sushi on me', 'ajo y oregano']
@@ -5,3 +7,30 @@ restaurants=['the south of france', 'pizza shop', 'sushi on me', 'ajo y oregano'
 transportations=['airplane', 'jet pack', 'boat','motorbike']
 
 entertainments=['muesuem', 'aquarium','comedy show','broadway show']
+
+
+destination=random.choice(destinations) 
+restaurant=random.choice(restaurants)
+transportation=random.choice(transportations)
+entertainment=random.choice(entertainments)
+
+
+def day_trip_generator(): 
+
+    day_trip=[destination,restaurant,transportation,entertainment]
+
+    while True: 
+
+        print(f'Destination:{day_trip[0].title()}')
+        print(f'Restaurant:{day_trip[1].title()}')
+        print(f'Transportation:{day_trip[2].title()}')
+        print(f'Entertainment:{day_trip[3].title()}')
+
+        prompt= ('Are you satisfied with your new trip?!')
+        prompt+= ("y/n")
+        prompt=input(prompt)
+        print(prompt)
+
+
+
+day_trip_generator()
